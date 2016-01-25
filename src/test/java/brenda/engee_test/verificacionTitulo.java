bea6a6495801a3033b894aebe1db1eacad24d0dd
fabianfalon <fabian.falon@gmail.com>
2016-01-24 23:56:09 -0300

    Commit inicial

---
+++ src/test/java/brenda/engee_test/verificacionTitulo.java
@@ -0,0 +1,24 @@
+package brenda.engee_test;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import junit.framework.Assert;
+
+public class verificacionTitulo {
+	@Test
+	public void verificarTitulo() {
+		// Instancia de firefoxdriver
+		WebDriver driver = new FirefoxDriver();
+
+		driver.navigate().to("http://www.engee.com.ar/");
+
+		Assert.assertTrue("El mensaje debe tener la palabra Engee", driver.getTitle().contains("Engee"));
+
+		driver.close();
+		driver.quit();
+
+	}
+
+}
